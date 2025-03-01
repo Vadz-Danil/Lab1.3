a702f6360bb652cd21fd9291b179a76ca6155acd
Vadz-Danil <vadz.danil@gmail.com>
2025-03-01 21:28:54 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,21 +1,34 @@
 import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         BinaryTree bt = new BinaryTree();
-        bt.add(new Student("Іваненко","Петро",2,12345,"Спорт"));
-        bt.add(new Student("Петров","Иван",3,45678,"Малювання"));
-        bt.add(new Student("Сидоренко","Олег",4,11111,"Шахи"));
-        bt.add(new Student("Коваленко","Марія",2,22222,"Спорт"));
-        bt.add(new Student("Петренко","Данило",2,99999,"Спорт"));
+        Scanner sc = new Scanner(System.in);
+        bt.add(new Student("Іваненко","Петро",2,110,"Спорт"));
+        bt.add(new Student("Петров","Иван",3,105,"Малювання"));
+        bt.add(new Student("Сидоренко","Олег",4,103,"Шахи"));
+        bt.add(new Student("Коваленко","Марія",2,107,"Спорт"));
+        bt.add(new Student("Петренко","Данило",2,190,"Спорт"));
+        bt.add(new Student("Бондар","Василь",5,195,"Музика"));
         bt.print();
-        System.out.println("\nСтуденти 2-го курсу,які займаються спортом:");
-        ArrayList<Student> result = bt.find(2,"Спорт");
-        for (Student student : result){
-            System.out.println(student);
+        System.out.println("\nВведіть курс пошуку:");
+        int searchCourse = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Введіть хоббі для пошуку:");
+        String searchHobby = sc.nextLine();
+        ArrayList<Student> foundStudents = bt.find(searchCourse,searchHobby);
+        if (foundStudents.isEmpty()) {
+            System.out.println("Студентів за даними критеріями не знайдено.");
         }
-        System.out.println("\nДерево після видалення студентів 2-го курса,які займаються спортом:");
+        else{
+            System.out.println("Студенти знайдені:");
+            for (Student student : foundStudents) {
+                System.out.println(student);
+            }
+        }
         bt.delete(2,"Спорт");
+        System.out.println("Після видалення:");
         bt.print();
-
     }
 }
